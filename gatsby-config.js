64745d631b6fe41f1d46d0821e6503a837aad5c8
OldGuo <young.guo@samsara.com>
2020-03-29 13:10:30 -0700

    Personalize titles, description, picture

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Young Guo`,
+    author: `Young Guo`,
+    description: `Young's playground.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`
+      twitter: `youngxguo`
     }
   },
   plugins: [
